8ff33ed264a3ca015d1f5455e0d4795652ba5714
taeyeon <tyhwang@suprema.co.kr>
2026-06-30 16:43:12 +0900

    ✨ 출근/퇴근 시각 수정 다이얼로그 추가

---
+++ tests/test_edit_dialog.py
@@ -0,0 +1,12 @@
+import pytest
+
+from widget.edit_dialog import build_iso
+
+
+def test_build_iso_ok():
+    assert build_iso("2026-06-30", "09:05") == "2026-06-30T09:05:00+09:00"
+
+
+def test_build_iso_invalid_raises():
+    with pytest.raises(ValueError):
+        build_iso("2026-06-30", "9시")
